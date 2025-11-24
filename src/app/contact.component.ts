4440919b32b6373512b01757a69dc186e1442ec5
Mahesh SK <maheshsk4212@gmail.com>
2025-11-24 22:34:48 +0530

    Fix typo: change nimport to import in contact.component.ts

--- src/app/contact.component.ts
+++ src/app/contact.component.ts
@@ -1,4 +1,4 @@
-nimport { Component, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
